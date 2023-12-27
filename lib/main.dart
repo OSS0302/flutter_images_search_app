2b701bcfa25bb106ea1882fbd2b023ecbc211813
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-27 17:10:39 +0900

    패키지 추가 main 화면 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app/ui/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
                                                                                                                                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:
+      home:const MainScreen(),
     );
   }
 }
