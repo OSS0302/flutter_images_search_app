2d20b15fe7f4a729d6997d984c8c4bbb921e8d54
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-28 23:24:06 +0900

    이미지 앱 api 작성

--- lib/23_12_28/data_source/pixabay_api.dart
+++ lib/23_12_28/data_source/pixabay_api.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:flutter_image_search_app/23_12_28/dto/pixabay_dto.dart';
 import 'package:flutter_image_search_app/key.dart';
+import 'package:http/http.dart' as http;
 
 class PixabayApi{
   Future<PixabayDto> getImagesResult(String query) async {
-    final apiResponse = await Uri.parse('https://pixabay.com/api/?key=$key=$query&image_type=photo');
-      return PixabayDto.from(jsonDecode(apiResponse.body));
+    final response = await http.get(Uri.parse('https://pixabay.com/api/?key=$key=$query&image_type=photo'));
+      return PixabayDto.fromJson(jsonDecode(response.body));
   }
 }
