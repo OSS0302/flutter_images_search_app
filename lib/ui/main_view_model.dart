f06a0398064a6b46d3924d361e63147dc1aaf06d
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-31 22:20:03 +0900

    stream을 이용한 mvvm 안에 viewModel 저굥ㅇ

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -1,19 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter_image_search_app/23_12_27/repository/image_item_repository.dart';
 
 import '../model/image_item.dart';
 
 class MainViewModel {
   final repository = PixabayImageItemRepository();
-  List<ImageItem> imageItems = [];
   List<ImageItem> _imageItems = [];
-  bool isLoading = false;
 
-  //수정 안되게 하는 리스트 (UnmodifiableListView)
-  List<ImageItem> get imageItemList => List.unmodifiable(_imageItems);
+  List<ImageItem> get imageItems => List.unmodifiable(_imageItems);
 
+  final _streamLoadingController = StreamController<bool>();
+  Stream<bool> get isLoadingStream => _streamLoadingController.stream;
 
   Future<void> searchImage(String query) async {
-    imageItems = await repository.getImageItems(query);
+
+    // 화면의 갱신
+    _streamLoadingController.add(true);
+
     _imageItems = await repository.getImageItems(query);
+
+    //화면의 갱신
+    _streamLoadingController.add(false);
   }
 }
