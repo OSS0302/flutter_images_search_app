ac110110a8a36dec7fd6c787d9d9d8ab31e41fb8
Ohsangsoo <tkdtn3264@gmail.com>
2024-01-03 00:01:18 +0900

    fix 이미지앱 클론 4일차 mapper 수정

--- lib/mapper/image_mapper.dart
+++ lib/mapper/image_mapper.dart
@@ -2,7 +2,10 @@ import 'package:flutter_image_search_app/dto/pixabay_dto.dart';
 import 'package:flutter_image_search_app/model/image_item.dart';
 
 extension DtoToModel on Hits {
-ImageItem toImageItem() {
-  return ImageItem(
-      imageUrl: previewURL ?? '이미지가 없습니다. 다시 확인해주세요';
-      tags:tags ?? '',
+  ImageItem toImageItem() {
+    return ImageItem(
+      imageUrl: previewURL ?? '이미지가 없습니다. 다시 확인해주세요',
+      tags: tags ?? '',
+    );
+  }
+}
