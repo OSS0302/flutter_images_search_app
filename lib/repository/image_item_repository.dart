5ed563b73202c3f9738b0157bf50f347c4d42a4d
Ohsangsoo <tkdtn3264@gmail.com>
2024-01-03 22:57:49 +0900

    클른하기 위해서 제거

--- lib/repository/image_item_repository.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:flutter_image_search_app/model/image_item.dart';
-// 인터페이스 ImageItemRepository
-abstract interface class ImageItemRepository {
-  Future<List<ImageItem>> getImageItems(String query);
-}
-
