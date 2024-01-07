54f4f58f24016e56acb065662994b3d11ec7e8e4
OSS0302 <tkdtn3264@gmail.com>
2024-01-07 18:23:24 +0900

    api 데이터 가져오는 주석 추가하기

--- lib/data/data_source/pixabay_api.dart
+++ lib/data/data_source/pixabay_api.dart
@@ -6,7 +6,7 @@ import 'package:flutter_image_search_app/key.dart';
 
 class PixabayApi {
   Future<PixabayDto> getImagesResult(String query) async {
-
+    //api 데이터 가져오기
     final response = await http.get(Uri.parse(
          'https://pixabay.com/api/?key=$key&q=$query&image_type=photo'));
     return PixabayDto.fromJson(jsonDecode(response.body));
